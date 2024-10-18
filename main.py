1ff054f138b1af4d33be992860344aec2a095f17
Altynay <77896777+AltynayA@users.noreply.github.com>
2024-10-19 01:09:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+curl \
+- H
+'Content-Type: application/json' \
+- d
+'{"contents":[{"parts":[{"text":"Explain how AI works"}]}]}' \
+- X
+POST
+'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=YOUR_API_KEY'
+
+
+ourapi = "AIzaSyBmm1ePvaV6_raiipE_gdbcvdupC5MD4IQ";
+
+#это ваще ненужно
+
+
+
+
---
+++ script.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("firstpage");
+button.addEventListener('click', function() {
+//console.log('I just moved a page')
+window.location.href = "pagetwo.html";
+});
+
+var button = document.getElementById("secondpage");
+button.addEventListener('click', function() {
+//console.log('I just moved a page')
+window.location.href = "pagethree.html";
+});
